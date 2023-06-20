b7c4d6764cab39c10ab1b8b7bdc011298fbc89a1
Amal221200 <amalmurikkoli2000@gmail.com>
2023-06-20 12:13:15 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import image from './assets/images/image-qr-code.png'
+function App() {
+
+  return (
+    <div className='container'>
+      <div className="box">
+        <div className="image">
+          <img src={image} alt="" srcSet='' />
+        </div>
+        <div className="content">
+          <h2>Improve your frontend <br /> skills by building projects</h2>
+          <p>Scan the QR code to visit Frontend <br />Mentor and take your skills to <br /> the next level</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
