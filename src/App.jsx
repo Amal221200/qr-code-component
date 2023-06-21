54a9850fb5d77200c34ab76c4e9dc2dcd7e83247
Amal221200 <amalmurikkoli2000@gmail.com>
2023-06-21 10:05:05 +0530

    img correction

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className='container'>
       <div className="box">
         <div className="image">
-          <img src={image} alt="" srcSet='' />
+          <img src={image} alt="This is a qr code to Frontend Mentor.io"  />
         </div>
         <div className="content">
           <h2>Improve your frontend <br /> skills by building projects</h2>
